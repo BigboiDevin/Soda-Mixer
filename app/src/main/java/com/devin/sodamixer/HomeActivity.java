c3cbb5e6f8b5aafd980cbad8f1afa12014a24e15
devin <devinzhong3746@gmail.com>
2018-06-02 12:46:01 -0700

    first commit

---
+++ app/src/main/java/com/devin/sodamixer/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.devin.sodamixer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
